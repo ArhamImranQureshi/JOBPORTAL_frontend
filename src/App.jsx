f194a14628c19754a9b1532ac990feec8446cdc3
Arham Qureshi <arhamgameslover@gmail.com>
2025-01-04 23:57:42 +0500

    login Signup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import './App.css'
 import Navbar from './components/ui/shared/Navbar'
-
+import Login from './components/ui/auth/Login'
+import Signup from './components/ui/auth/Signup'
+import { createBrowserRouter } from 'react-router-dom'
+const appRouter= createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/LOGIN',
+    element:<Login/>
+  },
+  {
+    path:'/',
+    element:<Signup/>
+  },
+])
 function App() {
   return (
     <>
