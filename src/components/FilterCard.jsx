605b395aedac55c8911586b3a07da77b1ce1e04d
Arham Qureshi <arhamgameslover@gmail.com>
2025-01-07 05:22:19 +0500

    jobs page completed

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import { Label } from "./ui/label";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import React from "react";
+
+const filterData = [
+  {
+    filterType: "Location",
+    array: ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"],
+  },
+  {
+    filterType: "Industry",
+    array: [
+      "Frontend",
+      "Backend",
+      "Fullstack",
+      "DevOps",
+      "Mobile Development",
+      "AI/ML",
+      "Data Science",
+      "Cyber Security",
+      "Game Development",
+      "Blockchain",
+      "IOT",
+      "Cloud Computing",
+      "UI/UX",
+      "Graphic Designing",
+      "Digital Marketing",
+      "Content Writing",
+      "SEO",
+      "SMM",
+      "SEM",
+    ],
+  },
+  {
+    filterType: "Salary",
+    array: [
+      "0-40K",
+      "40K-70K",
+      "70K-100K",
+      "100K-150K",
+      "150K-200K",
+      "200K-300K",
+    ],
+  },
+];
 
 const FilterCard = () => {
   return (
-    <div>
-      Filter
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter Jobs</h1>
+      <hr className="mt-3"></hr>
+      <RadioGroup>
+        {
+            filterData.map((data,index)=>(
+                <div>
+                    <h1 className="font-bold text-lg">{data.filterType}</h1>
+                    {
+                        data.array.map((items,index)=>{
+                            return(
+                                <div className="flex items-center space-x-2 m-2">
+                                    <RadioGroupItem value={items}/>
+                                    <Label>{items}</Label>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            ))
+        }
+      </RadioGroup>
     </div>
-  )
-}
+  );
+};
 
-export default FilterCard
+export default FilterCard;
