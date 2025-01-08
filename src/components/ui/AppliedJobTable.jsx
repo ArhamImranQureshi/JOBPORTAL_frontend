fd87d8f4a5353f6ae8de0a052894327b21a2805a
Arham Qureshi <arhamgameslover@gmail.com>
2025-01-08 05:37:29 +0500

    profilepage complete

--- src/components/ui/AppliedJobTable.jsx
+++ src/components/ui/AppliedJobTable.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './table'
+import { Badge } from './badge'
 
 const AppliedJobTable = () => {
   return (
     <div>
-      
+      <Table>
+        <TableCaption>Applied Jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {
+            [1,2].map((item,index)=>(
+              <TableRow  key={index}>
+                <TableCell>08-01-2024</TableCell>
+                <TableCell>Frontend-Developer</TableCell>
+                <TableCell>10Pearls</TableCell>
+                <TableCell className="text-right"><Badge>Selected</Badge></TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
     </div>
   )
 }
